1a9ac7cb37726b893639c6f6d350b35470f095fd
LICH <1529555652@qq.com>
2023-11-30 11:50:55 +0800

    add dataset coqa

--- llm_box/dataset/coqa.py
+++ llm_box/dataset/coqa.py
@@ -14,14 +14,38 @@ class Coqa(GenerationDataset):
     CoQA is a large-scale dataset for building Conversational Question Answering systems.
 
     Examples:
-        answers: 
-        questions: When was the Vat formally opened?
-        source: wikipedia
-        story: The Vatican Apostolic Library (), more commonly called the Vatican Library or simply the Vat, is the library of the Holy See, l..."
+        answers:
+               [['white', 'white', 'white', 'white'],
+                ['in a barn', 'in a barn', 'in a barn', 'in a barn near'],
+                ['no', 'no', 'No', 'no'],
+                ['with her mommy and 5 sisters', 'her mommy and 5 other sisters', 'her mommy and 5 other sisters', 'her mommy and 5 other sisters'],
+                ['orange and white', 'orange with white tiger stripes', 'orange', 'orange'],
+                ['no', 'no', 'No', 'no'],
+                ['she painted herself', 'she painted herself', 'paint herself like them', 'paint herself like them'],
+                ['the farmer', "the farmer's", "the old farmer's", "the farmer's"],
+                ['they started laughing', 'they started laughing', 'rubbed her face', 'started laughing'],
+                ['a bucket of water', 'dropped her into a big bucket of water', 'into a big bucket of water', 'a big bucket of water'],
+                ['licked her face', 'licked her face', 'licked her face', 'licked her face'],
+                ['no', 'no', 'No', 'no']]
+        questions: 
+               ['What color was Cotton?',
+                'Where did she live?',
+                'Did she live alone?',
+                'Who did she live with?',
+                'What color were her sisters?',
+                'Was Cotton happy that she looked different than the rest of her family?',
+                'What did she do to try to make herself the same color as her sisters?',
+                'Whose paint was it?',
+                "What did Cotton's mother and siblings do when they saw her painted orange?",
+                "Where did Cotton's mother put her to clean the paint off?",
+                'What did the other cats do when Cotton emerged from the bucket of water?',
+                'Did they want Cotton to change the color of her fur?']
+        source: 'mctest',
+        story:  'Once upon a time, in a barn near a farm house, there lived a little white kitten named Cotton. Cotton lived high up in a nice warm place above the barn where all of the farmer\'s horses slept. But Cotton wasn\'t alone in her little home above the barn, oh no. She shared her hay bed with her mommy and 5 other sisters. All of her sisters were cute and fluffy, like Cotton. But she was the only white one in the bunch. The rest of her sisters were all orange with beautiful white tiger stripes like Cotton\'s mommy. Being different made Cotton quite sad. She often wished she looked like the rest of her family. So one day, when Cotton found a can of the old farmer\'s orange paint, she used it to paint herself like them. When her mommy and sisters found her they started laughing. \n\n"What are you doing, Cotton?!" \n\n"I only wanted to be more like you". \n\nCotton\'s mommy rubbed her face on Cotton\'s and said "Oh Cotton, but your fur is so pretty and special, like you. We would never want you to be any other way". And with that, Cotton\'s mommy picked her up and dropped her into a big bucket of water. When Cotton came out she was herself again. Her sisters licked her face until Cotton\'s fur was all all dry. \n\n"Don\'t ever do that again, Cotton!" they all cried. "Next time you might mess up that pretty white fur of yours and we wouldn\'t want that!" \n\nThen Cotton thought, "I change my mind. I like being special".' 
     """
 
     name = "coqa"
-    instruction = "Answer the last question based the following story."
+    instruction = "Answer the last question based on the given passage."
 
     example_set = "train"
     evaluation_set = "validation"
@@ -29,39 +53,37 @@ class Coqa(GenerationDataset):
     load_args = ("coqa",)
 
     def _load_raw_dataset(self, dataset_path, subset_name, evaluation_set, example_set):
-        evaluation_dataset = json.load(open("./coqa_data/dev.json"))["data"][:10]
+        evaluation_dataset = json.load(open("./coqa_data/dev.json"))["data"][:50]
         example_dataset = json.load(open("./coqa_data/train.json"))["data"]
-        self.evaluation_data = Coqa.convert(evaluation_dataset, "dev")
-        self.example_data = Coqa.convert(example_dataset, "train")
+        self.evaluation_data = self.convert(evaluation_dataset, "dev")
+        self.example_data = self.convert(example_dataset, "train")
 
-    @staticmethod
-    def convert(raw_dataset, data_type):
+    def convert(self, raw_dataset, data_type):
         dataset = []
         for instance in raw_dataset:
-            a = {}
-            a["story"] = instance["story"]
-            a["id"] = instance["id"]
+            converted_instance = {}
+            converted_instance["story"] = instance["story"]
+            converted_instance["id"] = instance["id"]
             multiple_answers = [instance["answers"]]
             if data_type == "dev":
                 multiple_answers += instance["additional_answers"].values()
-            a["questions"] = []
-            a["answers"] = []
+            converted_instance["questions"] = []
+            converted_instance["answers"] = []
             for i, qa in enumerate(instance["questions"]):
-                a["questions"].append(qa["input_text"])
+                converted_instance["questions"].append(qa["input_text"])
                 answer_list = []
                 for answers in multiple_answers:
                     answer = answers[i]
                     answer_list.append(answer["input_text"])
-                a["answers"].append(answer_list)
-            dataset.append(a)
+                converted_instance["answers"].append(answer_list)
+            dataset.append(converted_instance)
         return dataset
 
     def format_instance(self, instance):
         num_questions = len(instance["questions"])
         return self._format_instances(instance, num_questions - 1)
 
-    @staticmethod
-    def normalize_answer(s):
+    def normalize_answer(self, s):
         """Lower text and remove punctuation, stories and extra whitespace."""
 
         def remove_articles(text):
@@ -79,29 +101,30 @@ class Coqa(GenerationDataset):
             return text.lower()
 
         return white_space_fix(remove_articles(remove_punc(lower(s))))
-
-    @staticmethod
-    def get_tokens(s):
+    
+    def get_tokens(self, s):
         if not s: return []
-        return word_tokenize(Coqa.normalize_answer(s))
+        return word_tokenize(self.normalize_answer(s))
 
-    @staticmethod
-    def compute_exact(a_gold, a_pred):
-        return int(Coqa.normalize_answer(a_gold) == Coqa.normalize_answer(a_pred))
-
-    @staticmethod
-    def calculate_f1_score(reference, predicted):
-        pattern = r'[\n]'  # 正则表达式模式，匹配`.`、`!` 或者换行符（`\n`）
-        match = re.search(pattern, predicted)  # 在字符串中搜索第一个匹配项
+    def compute_exact(self, reference, predicted):
+        pattern = r'[.!(\n)]'
+        match = re.search(pattern, predicted) 
         if match:
             index = match.start()
             predicted = predicted[:index]
-        gold_toks = Coqa.get_tokens(reference)
-        pred_toks = Coqa.get_tokens(predicted)
+        return int(self.normalize_answer(reference) == self.normalize_answer(predicted))
+
+    def calculate_f1_score(self, reference, predicted):
+        pattern = r'[.!(\n)]'
+        match = re.search(pattern, predicted) 
+        if match:
+            index = match.start()
+            predicted = predicted[:index]
+        gold_toks = self.get_tokens(reference)
+        pred_toks = self.get_tokens(predicted)
         common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
         num_same = sum(common.values())
         if len(gold_toks) == 0 or len(pred_toks) == 0:
-            # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
             return int(gold_toks == pred_toks)
         if num_same == 0:
             return 0
@@ -112,19 +135,13 @@ class Coqa(GenerationDataset):
 
     def calculate_metric(self, predictions):
         f1_sum = 0
+        em_sum = 0
         for prediction, reference in zip(predictions, self.references):
-            f1_sum += max(Coqa.calculate_f1_score(answer, prediction) for answer in reference)
-        # res_list = np.array(list(map(lambda _p: Drop.calculate_f1_score(_p[0],_p[1]), zip(self.references,predictions))))
-        print(f1_sum)
-        print(len(predictions))
-        return {'F1 score: ': f1_sum / len(predictions)}
+            f1_sum += max(self.calculate_f1_score(answer, prediction) for answer in reference)
+            em_sum += max(self.compute_exact(answer, prediction) for answer in reference)
+        return {'F1 score:': f1_sum / len(predictions),"em score": em_sum / len(predictions)}
 
     def construct_instances(self):
-        r"""Construct and format all the instances of `evaluation_data`.
-
-        Returns:
-            List[str]: The list of final formatted instances.
-        """
         self.evaluation_instances = []
         for instance in self.evaluation_data:
             formatted_instances = self.format_mul_instances(instance)
