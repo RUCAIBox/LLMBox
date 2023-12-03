7f6c7ab4c86e4685cb17ec92c1d8695ac11dcb41
LICH <1529555652@qq.com>
2023-12-03 21:03:59 +0800

    update the text

--- llm_box/dataset/squad_v2.py
+++ llm_box/dataset/squad_v2.py
@@ -5,13 +5,14 @@ import numpy as np
 
 
 class Squad_v2(GenerationDataset):
-    """The dataset of GSM8K.
+    """The dataset of Squad_v2.
 
-    GSM8K(Cobbe et al. 2021), linguistically diverse grade school math word problems
+    Gcombines the 100,000 questions in SQuAD1.1 with over 50,000 unanswerable questions written adversarially by crowdworkers to look similar to answerable ones. 
 
     Examples:
-        question: Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?
-        answer: Natalia sold 48/2 = <<48/2=24>>24 clips in May. Natalia sold 48+24 = <<48+24=72>>72 clips altogether in April and May. #### 72
+        context: The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse ("Norman" comes from "Norseman") raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries.
+        question: In what country is Normandy located?
+        answer: ['France', 'France', 'France', 'France']
     """
 
 
