c882a98f77197ab35d55c0404c41f379890d6fa3
wangjiapeng1010 <wangjp1010@tju.edu.cn>
2023-12-06 11:00:36 +0800

    fix detail

--- llm_box/metric/accuracy.py
+++ llm_box/metric/accuracy.py
@@ -6,14 +6,7 @@ class Accuracy(Metric):
     r""" Calculate the Accuracy score.
     """
 
-    def __init__(self, answer_type='str'):
-        self.answer_type = answer_type
-
-    def __call__(self, predictions, references):
-        if self.answer_type == 'str':
-            score_list = np.asarray(predictions) == np.asarray(references)
-        elif self.answer_type == 'float':
-            predictions = [float(p) if p.replace('.', '', 1).isdigit() else 0.0 for p in predictions]
-            references = [float(r) for r in references]
-            score_list = np.isclose(predictions, references)
+    @staticmethod
+    def __call__(predictions, references):
+        score_list = np.asarray(predictions) == np.asarray(references)
         return {'Accuracy': np.mean(score_list) * 100}
