5623c42495ffcd341ba109fcfe38a43f9695b48c
Xuan_xcx <xiachunxuan@ruc.edu.cn>
2024-01-07 14:18:36 +0800

    change the wmt16 dataset

--- llm_box/dataset/wmt16.py
+++ llm_box/dataset/wmt16.py
@@ -14,11 +14,6 @@ class Wmt16(GenerationDataset):
         hypothesis: Obama welcomes Netanyahu
         reference: Obama receives Netanyahu
     """
-    def __init__(self, config, args: DatasetArguments, model: Model, subset_name: str | None = None):
-        super().__init__(args, model, subset_name)
-        self.config = config
-        instruction = f"Translate from {self.config[:2]} to {self.config[3:5]}"
-        load_args = ("wmt16", self.config)
     
     name = 'wmt16'
     metric = "bleu"
@@ -27,7 +22,24 @@ class Wmt16(GenerationDataset):
     example_set = "train"
     metrics = [Bleu()]
     
+    instruction = ''
+    load_args = ('wmt16',)
+    
+    def __init__(self, args: DatasetArguments, model: Model, subset_name: Optional[str] = None):
+        # pdb.set_trace()
+        self.config = ' '
+        self.config = subset_name
+        raw_config = self.config if self.config[3:] == "en" else self.config[3:5] + "-" + self.config[:2]
+        Wmt16.instruction = f"Translate from {self.config[:2]} to {self.config[3:5]}"
+        # Wmt16.load_args = ("wmt16", raw_config)
+        print(f"Init wmt16, config = {self.config}, raw_config = {raw_config}")
+        
+        super().__init__(args, model, subset_name)
+    
     def format_instance(self, instance):
+        # print(f'instance is {instance}')
+        # print(f"in instance, config = {self.config}")
+        instance = instance['translation']
         source_text = instance[self.config[:2]]
         target_text = instance[self.config[3:5]]
         return dict(source=source_text, target=target_text)
