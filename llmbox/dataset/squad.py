6192f0041cdd434612543ad1eb49a0f5c3f27300
lich <18949698451.163.com>
2024-01-23 13:47:32 +0800

    add dataset squad

---
+++ llmbox/dataset/squad.py
@@ -0,0 +1,92 @@
+import random
+import re
+
+import numpy as np
+
+from ..metric import F1, Em
+from .generation_dataset import GenerationDataset
+
+
+class Squad(GenerationDataset):
+    """The dataset of Squad.
+
+    Stanford Question Answering Dataset (SQuAD) is a reading comprehension dataset, consisting of questions posed by crowdworkers on a set of Wikipedia articles, where the answer to every question is a segment of text, or span, from the corresponding reading passage.
+
+    Examples:
+        context: Architecturally, the school has a Catholic character. Atop the Main Building\'s gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend "Venite Ad Me Omnes". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.
+        question: To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?
+        answer: ['Saint Bernadette Soubirous']
+    """
+
+    instruction = 'Answer each question using information in the preceding background paragraph.'
+    example_set = "train"
+    evaluation_set = "validation"
+    load_args = ("squad",)
+    metrics = [F1(), Em()]
+    extra_model_args = dict(max_tokens=64, temperature=0, stop=["\n"])
+
+    def format_instance(self, instance):
+        source_text = (
+            "Title: " + instance["title"] + "\n\nBackground: " + instance["context"] + "\n\nQ: " +
+            instance["question"] + "\n\nA:"
+        )
+        target_text = " " + instance["answers"]["text"][0]
+        return dict(source=source_text, target=target_text)
+
+    def construct_examples(self, instance=None) -> str:
+        r"""Format one instance with the instruction and demonstration.
+
+        Args:
+            instance (Dict): a pre-formatted evaluation instance.
+
+        Returns:
+            str: The constructed demonstration text.
+        """
+        if self.num_shots == 0:
+            return ""
+        elif len(self.example_data) == 0:
+            raise ValueError(
+                f"Receive num_shots={self.num_shots}, but cannot construct examples for dataset {self.name} without example data."
+            )
+
+        generation_example_text = ""
+        generation_example_token_nums = 0
+        classified_title = {}
+        for item in self.example_data:
+            key = (item["title"], item["context"])
+            if key in classified_title:
+                classified_title[key].append(item)
+            else:
+                classified_title[key] = [item]
+
+        keys = list(classified_title.keys())
+        randoms_keys = [keys[i] for i in np.random.choice(range(len(keys)), self.num_shots, replace=False)]
+        for data in [classified_title[key] for key in randoms_keys]:
+            instance = data[0]
+            source_text = "Title: " + instance["title"] + "\n\nBackground: " + instance["context"]
+            random.shuffle(data)
+            for instance in data:
+                source_text += "\n\nQ: " + instance["question"] + "\n\nA:"
+                target_text = " " + instance["answers"]["text"][0]
+                source_text += target_text
+            cur_example_text = source_text + "\n\n"
+            cur_token_num = len(self.tokenizer.encode(cur_example_text))
+            if cur_token_num + generation_example_token_nums <= self.max_example_tokens:
+                generation_example_text += cur_example_text
+                generation_example_token_nums += cur_token_num
+        return generation_example_text
+
+    @property
+    def references(self):
+        return [instance["answers"]["text"] for instance in self.evaluation_data]
+
+    def post_processing(self, preds):
+        predictions = []
+        pattern = r"[.!(\n)]"
+        for pred in preds:
+            match = re.search(pattern, pred)
+            if match:
+                index = match.start()
+                pred = pred[:index]
+            predictions.append(pred)
+        return predictions
