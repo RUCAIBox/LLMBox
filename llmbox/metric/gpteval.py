169599ec6fc2a9e2db7635689b1dfbc39ed4f8b4
wangjp1010 <wangjp1010@tju.edu.cn>
2024-02-18 18:06:54 +0800

    add ref answer

--- llmbox/metric/gpteval.py
+++ llmbox/metric/gpteval.py
@@ -9,6 +9,7 @@ from ..model import load_model
 logger = getLogger(__name__)
 
 JUDGE_PROMPT = "[Instruction]\nPlease act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of the response. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, you must rate the response on a scale of 1 to 10 by strictly following this format: \"[[rating]]\", for example: \"Rating: [[5]]\".\n\n[Question]\n{question}\n\n[The Start of Assistant's Answer]\n{answer}\n[The End of Assistant's Answer]"
+JUDGE_PROMPT_MATH = "[Instruction]\nPlease act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below. Your evaluation should consider correctness and helpfulness. You will be given a reference answer and the assistant's answer. Begin your evaluation by comparing the assistant's answer with the reference answer. Identify and correct any mistakes. Be as objective as possible. After providing your explanation, you must rate the response on a scale of 1 to 10 by strictly following this format: \"[[rating]]\", for example: \"Rating: [[5]]\".\n\n[Question]\n{question}\n\n[The Start of Reference Answer]\n{ref_answer_1}\n[The End of Reference Answer]\n\n[The Start of Assistant's Answer]\n{answer}\n[The End of Assistant's Answer]"
 
 score_pattern = re.compile("\[\[(\d+\.?\d*)\]\]")
 score_pattern_backup = re.compile("\[(\d+\.?\d*)\]")
@@ -30,7 +31,10 @@ class GPTEval(Metric):
         model.set_generation_args()
         responses = []
         for pred, refer in tqdm(zip(predictions, references), desc="Judging", total=len(predictions)):
-            user_prompt = JUDGE_PROMPT.format(question=refer, answer=pred)
+            if "ref_answer_1" not in refer:
+                user_prompt = JUDGE_PROMPT.format(question=refer["turns"][0], answer=pred)
+            else:
+                user_prompt = JUDGE_PROMPT_MATH.format(question=refer["turns"][0], ref_answer_1=refer["ref_answer_1"], answer=pred)
             responses.extend(model.generation([user_prompt]))
 
         ratings = []
