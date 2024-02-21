abf104d103038b59344c09287ce36cf2601759c9
huyiwen <1020030101@qq.com>
2024-02-21 22:09:14 +0800

    Fix winogrande

--- llmbox/dataset/winogrande.py
+++ llmbox/dataset/winogrande.py
@@ -1,5 +1,6 @@
-import numpy as np
 from logging import getLogger
+
+import numpy as np
 
 from .multiple_choice_dataset import MultipleChoiceDataset
 
@@ -27,57 +28,14 @@ class WinoGrande(MultipleChoiceDataset):
     load_args = ("winogrande", "winogrande_debiased")  # specify subset from command line
 
     def format_instance(self, instance):
-        text = instance['sentence'].split('_')
-        source_text = [text[0] + instance[option] for option in ['option1', 'option2']]
-        options = [text[1]] * 2
+        question, postfix = instance['sentence'].split('_')
+        options = [instance["option1"] + postfix, instance["option2"] + postfix]
         return dict(
-            source=source_text,
-            target=source_text[int(instance["answer"]) - 1],
+            source=question.strip(),
+            target_idx=int(instance["answer"]) - 1,
             options=options,
         )
 
     @property
     def references(self):
         return [int(instance["answer"]) - 1 for instance in self.evaluation_data]
-
-    def construct_examples(self, instance=None) -> str:
-        if self.num_shots == 0:
-            return ""
-        indice = self.random_indice
-        example_text = ""
-        example_token_nums = 0
-        for index in indice:
-            if hasattr(self, "formatted_example_data"):
-                example = self.formatted_example_data[index]
-            else:
-                example = self.format_instance(self.example_data[index])
-            cur_example_text = self.args.instance_format.format(
-                source=example["target"], target=example["options"][0]
-            ) + "\n\n"
-            cur_token_num = len(self.tokenizer.encode(cur_example_text))
-            if cur_token_num + example_token_nums <= self.max_example_tokens:
-                example_text += cur_example_text
-                example_token_nums += cur_token_num
-
-        return example_text
-
-    def construct_instances(self):
-        self.evaluation_instances = []
-        self.option_nums = []
-        for formatted_instance in self.formatted_evaluation_data:
-            for source, option in zip(formatted_instance['source'], formatted_instance['options']):
-                if self.examples == "":
-                    self.examples = self.construct_examples()
-                if self.model.type == "base":
-                    source = self.examples + self.args.instance_format.format(source=source, target="")
-                elif self.model.type == "instruction":
-                    source = (
-                        self.instruction + "\n\n" + self.examples +
-                        self.args.instance_format.format(source=source, target="")
-                    )
-                self.evaluation_instances.append((source, option))
-            self.option_nums.append(2)
-
-        logger.info("Evaluation mode: calculate PPL of the optional text based on the source text")
-        logger.info("Formatted example (source)\n" + self.evaluation_instances[0][0])
-        logger.info("Formatted example (option)\n" + self.evaluation_instances[0][1])
