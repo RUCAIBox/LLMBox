bc998b29a2589ec98e04bad7f6128acbd3acbbac
wangjp1010 <wangjp1010@tju.edu.cn>
2024-02-28 20:24:55 +0800

    change examples

--- llmbox/dataset/mbpp.py
+++ llmbox/dataset/mbpp.py
@@ -27,6 +27,10 @@ class Mbpp(GenerationDataset):
         super().__init__(args, model, subset_name=subset_name)
         self.metrics = [PassAtK(k=args.pass_at_k)]
 
+    def load_raw_dataset(self, dataset_path, subset_name, evaluation_set, example_set):
+        super().load_raw_dataset(dataset_path, subset_name, evaluation_set, example_set)
+        self.example_data = EXAMPLARS
+
     def format_instance(self, instance):
         prompt = instance["text"]
         tests = '\n'.join(instance["test_list"])
@@ -45,3 +49,7 @@ class Mbpp(GenerationDataset):
     @property
     def references(self):
         return self.evaluation_data
+
+EXAMPLARS = [{"text": "Write a function to find the similar elements from the given two tuple lists.", "code": "def similar_elements(test_tup1, test_tup2):\r\n  res = tuple(set(test_tup1) & set(test_tup2))\r\n  return (res) ", "task_id": 2, "test_setup_code": "", "test_list": ["assert similar_elements((3, 4, 5, 6),(5, 7, 4, 10)) == (4, 5)", "assert similar_elements((1, 2, 3, 4),(5, 4, 3, 7)) == (3, 4)", "assert similar_elements((11, 12, 14, 13),(17, 15, 14, 13)) == (13, 14)"], "challenge_test_list": []},
+             {"text": "Write a python function to identify non-prime numbers.", "code": "import math\r\ndef is_not_prime(n):\r\n    result = False\r\n    for i in range(2,int(math.sqrt(n)) + 1):\r\n        if n % i == 0:\r\n            result = True\r\n    return result", "task_id": 3, "test_setup_code": "", "test_list": ["assert is_not_prime(2) == False", "assert is_not_prime(10) == True", "assert is_not_prime(35) == True"], "challenge_test_list": []},
+             {"text": "Write a function to find the largest integers from a given list of numbers using heap queue algorithm.", "code": "import heapq as hq\r\ndef heap_queue_largest(nums,n):\r\n  largest_nums = hq.nlargest(n, nums)\r\n  return largest_nums", "task_id": 4, "test_setup_code": "", "test_list": ["assert heap_queue_largest( [25, 35, 22, 85, 14, 65, 75, 22, 58],3)==[85, 75, 65] ", "assert heap_queue_largest( [25, 35, 22, 85, 14, 65, 75, 22, 58],2)==[85, 75] ", "assert heap_queue_largest( [25, 35, 22, 85, 14, 65, 75, 22, 58],5)==[85, 75, 65, 58, 35]"], "challenge_test_list": []}]
