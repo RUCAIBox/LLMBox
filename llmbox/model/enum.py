73cb1a2b59e19acb67b851832e93de3b65177d40
CoderBak <hx-sun@outlook.com>
2024-03-12 13:22:32 +0800

    Add GPT-4

--- llmbox/model/enum.py
+++ llmbox/model/enum.py
@@ -1,5 +1,5 @@
 OPENAI_COMPLETION_MODELS = ["babbage-002", "davinci-002", "gpt-3.5-turbo-instruct"]
-OPENAI_CHAT_MODELS = ["gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k"]
+OPENAI_CHAT_MODELS = ["gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-4"]
 OPENAI_MODELS = OPENAI_COMPLETION_MODELS + OPENAI_CHAT_MODELS
 OPENAI_INSTRUCTION_MODELS = ["gpt-3.5-turbo-instruct"] + OPENAI_CHAT_MODELS
 ANTHROPIC_MODELS = ["claude-2.1", "claude-instant-1.2"]
