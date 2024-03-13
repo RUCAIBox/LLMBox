e5eb2bf3bbfd2a9022f439ecc90747438ba86fc3
LICH226 <ymiemie26@outlook.com>
2024-03-13 18:20:10 +0800

    add dataset penguins_in_a_table

--- llmbox/dataset/penguins_in_a_table.py
+++ llmbox/dataset/penguins_in_a_table.py
@@ -2,17 +2,16 @@ from .multiple_choice_dataset import MultipleChoiceDataset
 from ..metric import Accuracy
 import numpy as np
 
-class Penguins_in_a_table(MultipleChoiceDataset):
-    """The dataset of Copa.
 
-    The Choice Of Plausible Alternatives (COPA, Roemmele et al., 2011) dataset is a causal reasoning task in which a system is given a premise sentence and two possible alternatives.
+class Penguins_in_a_table(MultipleChoiceDataset):
+    """The dataset of Bigbench Penguins_in_a_table.
+
+    BIG-Bench but it doesn't require the hellish dependencies (tensorflow, pypi-bigbench, protobuf) of the official version.
 
     Example:
-        premise: The man turned on the faucet.
-        choice1: The toilet filled with water.
-        choice2: Water flowed from the spout.
-        question: effect
-        label: 1
+        inputs: Here is a table where the first line is a header and each subsequent line is a penguin: name, age, height (cm), weight (kg) Louis, 7, 50, 11 Bernard, 5, 80, 13 Vincent, 9, 60, 11 Gwen, 8, 70, 15 For example: the age of Louis is 7, the weight of Gwen is 15 kg, the height of Bernard is 80 cm. We now add a penguin to the table: James, 12, 90, 12 And here is a similar table, but listing giraffes: name, age, height (cm), weight (kg) Jody, 5, 430, 620 Gladys, 10, 420, 590 Marian, 2, 310, 410 Donna, 9, 440, 650 Which is the oldest penguin? Answer:	
+        multiple_choice_targets: [ "Louis", "Bernard", "Vincent", "Gwen", "James" ]
+        multiple_choice_scores: [ 0, 0, 0, 0, 1 ]
     """
 
     instruction = ""
