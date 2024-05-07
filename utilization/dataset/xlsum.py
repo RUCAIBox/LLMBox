dfab0921add7a73b8f9a9d6a0c242efc5f746a74
LICH226 <ymiemie26@outlook.com>
2024-05-07 10:45:59 +0800

    add dataset xlsum

---
+++ utilization/dataset/xlsum.py
@@ -0,0 +1,29 @@
+from ..metric import Rouge
+from .generation_dataset import GenerationDataset
+
+
+class Xlsum(GenerationDataset):
+    """The dataset of Xlsum.
+
+    We present XLSum, a comprehensive and diverse dataset comprising 1.35 million professionally annotated article-summary pairs from BBC, extracted using a set of carefully designed heuristics. The dataset covers 45 languages ranging from low to high-resource, for many of which no public dataset is currently available. XL-Sum is highly abstractive, concise, and of high quality, as indicated by human and intrinsic evaluation.
+
+    Examples:
+        text: The move is in response to an £8m cut in the subsidy received from the Department of Employment and Learning (DEL).\nThe cut in undergraduate places will come into effect from September 2015.\nJob losses will be among both academic and non-academic staff and Queen\'s says no compulsory redundancies should be required.\nThere are currently around 17,000 full-time undergraduate and postgraduate students at the university, and around 3,800 staff.\nQueen\'s has a current intake of around 4,500 undergraduates per year.\nThe university aims to reduce the number of student places by 1,010 over the next three years.\nThe BBC understands that there are no immediate plans to close departments or courses, but that the cuts in funding may put some departments and courses at risk.\nThe Education Minister Stephen Farry said he recognised that some students might now choose to study in other areas of the UK because of the cuts facing Northern Ireland\'s universities.\n"Some people will now be forced to look to opportunities in other parts of Great Britain and may not return to our economy," he said.\n"Defunding our investment in skills, particularly at a time when we\'re trying to grow the economy does not make a lot of sense. What\'s happening is we\'re going backwards.\n"The loss of any place is damaging to our economy, all subjects teach our young people critical skills."\nQueen\'s vice-chancellor Patrick Johnston said the cuts had the potential to damage the reputation of the university.\n"The potential negative impact, not just on the university but on the local economy is very significant," he said.\n"It\'s the last thing we want to do, but we have to begin to focus on those areas where we can grow the organisation and develop it - it\'s clear we can no longer depend on the public purse to fund tuition.\n"If we\'re not competitive we will not attract the best students, and we will not attract the best staff."\nJust under £100m, a third of the university\'s income, comes from the Northern Ireland Executive.\nDEL\'s budget was reduced by £62m earlier this year, and its budget for higher education institutions fell from £203m to £186m, a reduction of 8.2%.\nUlster University announced in February that it was dropping 53 courses.\nIt will be cutting jobs and student places, but it has not yet revealed how many.
+        summary: Queen's University Belfast is cutting 236 jobs and 290 student places due to a funding reduction.
+    """
+
+    instruction = ""
+    evaluation_set = "test"
+    example_set = "train"
+    metrics = [Rouge()]
+    load_args = ("csebuetnlp/xlsum",)
+    extra_model_args = dict(temperature=0)
+
+    def format_instance(self, instance):
+        source = instance["text"] + "\n\nTL;DR: "
+        target = instance["summary"]
+        return dict(source=source, target=target)
+
+    @property
+    def references(self):
+        return [instance["summary"] for instance in self.evaluation_data]
