74437cf42404e3bed8e9673386c2b1f89c7206c1
huyiwen <1020030101@qq.com>
2024-05-10 11:57:05 +0800

    catch error

--- utilization/utils/catch_error.py
+++ utilization/utils/catch_error.py
@@ -2,6 +2,11 @@ from traceback import format_exc
 
 from .logging import getFileLogger
 
+ERROR_OVERVIEW = {
+    "probability tensor contains either `inf`, `nan` or element < 0":
+    "probability tensor contains either `inf`, `nan` or element < 0.\nSee https://github.com/meta-llama/llama/issues/380 for more details."
+}
+
 
 def catch_error(func):
     """Catch the error and log the error message to log file."""
@@ -12,6 +17,9 @@ def catch_error(func):
         except Exception as e:
             file_logger = getFileLogger(func.__module__)
             file_logger.error(f"[{func.__name__}] {e.__class__.__name__}: {e}\n\n{format_exc()}")
+            for error, msg in ERROR_OVERVIEW.items():
+                if error in str(e):
+                    raise RuntimeError(e.__class__.__name__ + ": " + msg) from e
             raise e
 
     return wrapper
